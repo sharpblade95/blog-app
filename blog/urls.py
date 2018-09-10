10394bbd18be1dfac35f4d70edfc7c39efc20a7f
foozhan <f.ataiefard@gmail.com>
2018-09-10 11:04:23 +0430

    tests for CRUD added

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from .views import BlogListView,BlogDetailView,BlogCreateView,BlogUpdateView,Blo
 urlpatterns= [
 	path('', BlogListView.as_view(),name = 'home'),
 	path('post/<int:pk>/',BlogDetailView.as_view(),name='post_detail'),
-	path('post/new',BlogCreateView.as_view(),name='post_new'),
-	path('post/<int:pk>/edit',BlogUpdateView.as_view(), name='post_edit'),
+	path('post/new/',BlogCreateView.as_view(),name='post_new'),
+	path('post/<int:pk>/edit/',BlogUpdateView.as_view(), name='post_edit'),
 	path('post/<int:pk>/delete/',BlogDeleteView.as_view(), name='post_delete'),
 ]
